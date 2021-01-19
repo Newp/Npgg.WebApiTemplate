8b1b0e29931fc93157bac7a8494deaba90e32fd3
Newp <mynewp@gmail.com>
2021-01-19 22:10:20 +0900

    extension으로 entity 설정 단순화

--- src/WebApiExample/Service/SqlContext.cs
+++ src/WebApiExample/Service/SqlContext.cs
@@ -1,15 +1,34 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using System.Text.Json;
 
 namespace WebApiExample
 {
-    public class SqlContext :DbContext
+
+    public class KeyValue
     {
+        [Key]
+        public string Key { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public DateTime UpdateTime { get; set; } = DateTime.MinValue;
+    }
+
+    public class SqlContext : DbContext
+    {
+        public DbSet<KeyValue>? KeyValues { get; set; }
+
         public SqlContext(DbContextOptions options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            this.SetJsonConverter(modelBuilder);
         }
     }
 
