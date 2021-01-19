89239df83fa6abb4f2bb36e966e1147bd8a750a2
Newp <mynewp@gmail.com>
2021-01-19 22:07:28 +0900

    efcore에서 모든 property에 대해서 primitive type이 아니거나 string/datetime 이 아니라면 json으로 변환하는 기능 추가

---
+++ src/WebApiExample/Extensions/EFCoreExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiExample
+{
+    public static class EFCoreExtensions
+    {
+        public static void SetJsonConverter<Context>(this Context context, ModelBuilder modelBuilder) where Context : DbContext
+        {
+            var dbsetType = typeof(DbSet<>);
+
+            var list = context.GetType().GetProperties()
+                .Select(property => property.PropertyType)
+                .Where(property => property.IsGenericType && dbsetType.IsAssignableFrom(property.GetGenericTypeDefinition()))
+                .Select(property => property.GetGenericArguments().First())
+                .ToArray();
+
+            var valueConverterType = typeof(EFCoreExtensions)
+                .GetMethod(nameof(CreateJsonConverter), System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+                ?? throw new Exception($"not found {nameof(CreateJsonConverter)}");
+
+            foreach (var type in list)
+            {
+                var entity = modelBuilder.Entity(type);
+
+                var notPrimitiveMembers = type.GetProperties()
+                    .Where(property => property.PropertyType.IsPrimitive == false
+                    && property.PropertyType != typeof(DateTime)
+                    && property.PropertyType != typeof(string))
+                    .ToArray();
+
+
+                foreach (var property in notPrimitiveMembers)
+                {
+                    var converterGenerator = valueConverterType.MakeGenericMethod(property.PropertyType);
+
+                    var converter = (ValueConverter)converterGenerator.Invoke(context, null)!;
+
+                    entity.Property(property.Name)?.HasConversion(converter);
+                }
+            }
+        }
+        static ValueConverter CreateJsonConverter<T>() => new ValueConverter<T, string>(
+                instance => JsonConvert.SerializeObject(instance)
+                , json => JsonConvert.DeserializeObject<T>(json)
+            );
+    }
+}
